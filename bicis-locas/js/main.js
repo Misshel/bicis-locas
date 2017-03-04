09534e50212cb4c041b1b60fcf91dc5f19a96654
Misshel <jenmish094@gmail.com>
2017-03-04 12:43:27 -0500

    mi primera copia

--- bicis-locas/js/main.js
+++ bicis-locas/js/main.js
@@ -1,3 +1,29 @@
+//creando mi funcion para errores
+ function error(){
+   alert("Cometio un error");
+ }
+
 function validateForm(){
-	/* Escribe tú código aquí */
+//Obteniendo los valores ingresados
+var nombre = document.getElementById("name").value;
+var apellido = document.getElementById("lastname").value;
+var email = document.getElementById("input-email").value;
+var contraseña= document.getElementById("input-password").value;
+
+//condiciones
+var letras =/([A-Z]{1}[a-z]+)|\s([A-Z]{1}[a-z]*)$/ ;
+var num=/^\d{6,}$/;
+var corrreo= /^(\w+\w-\d*)*\@[a-z]+\.[a-z]{2,3}$/;
+
+//validando nombre y apellido
+
+  if(letras.test(nombre) && letras.test(apellido) && corrreo.test(email) && num.test(contraseña)){
+    if(letras.test(nombre)== false||letras.test(apellido)==false){
+      alert("Ingresar datos con la primera letra Mayuscula y solo un espacio entre letras."+"<br>"+"Por favor");
+    }
+  }
+  else {
+    error();
+  }
+
 }
